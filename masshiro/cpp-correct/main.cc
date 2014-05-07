5c6681c908fe025176f25019e1e466d033cf4f79
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2014-05-08 08:02:51 +0900

    issue #14: masshiroi-world in/outs

---
+++ masshiro/cpp-correct/main.cc
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+double x[10], y[10];
+int ix[10];
+
+double dist(double x1, double y1, double x2, double y2) {
+    double x = x1 - x2;
+    double y = y1 - y2;
+    return sqrt(x*x + y*y);
+}
+
+int main() {
+    int n;
+    for (;;) {
+        cin >> n;
+        if (n == -1) return 0;
+        for (int i = 0; i < n; i++) {
+            cin >> x[i] >> y[i];
+            ix[i] = i;
+        }
+        x[n] = 0;
+        y[n] = 0;
+        double ans = 1e100;
+        do {
+            double d = dist(0, 0, x[0], y[0]);
+            for (int i = 0; i < n; i++) {
+                d += dist(x[i], y[i], x[i+1], y[i+1]);
+            }
+            if (ans > d) {
+                ans = d;
+            }
+        } while (next_permutation(ix, ix+n));
+        printf("%.3f\n", ans);
+    }
+}
---
+++ masshiro/tests/validator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../lib')
+from checker import Checker
+
+def main():
+  ch = Checker()
+
+  while True:
+    n = int(ch.readLine())
+    if n == -1:
+      break
+    ch.checkRange(n, 1, 5+1, 'n')
+    ps = set()
+    ps.add((0, 0))
+    for _ in range(n):
+      x, y = [int(w) for w in ch.readLine().strip().split()]
+      ch.check((x, y) not in ps, "no dup points")
+      ch.checkRange(x, -50, 50+1, 'x')
+      ch.checkRange(y, -50, 50+1, 'y')
+      ps.add((x, y))
+
+if __name__ == '__main__':
+  main()
