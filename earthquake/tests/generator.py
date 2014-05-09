1a292792d21aae1707ae0f7b84a7fce7663a5a98
ss0201 <ss0201.777@gmail.com>
2014-05-10 00:56:30 +0900

    Fix the input generator of 'earthquake' so that it produces inputs for various answers

--- earthquake/tests/generator.py
+++ earthquake/tests/generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 
 import random
+import math
 
 def generate(index):
   filename = '50-random%02d.in' % index
@@ -8,10 +9,12 @@ def generate(index):
     for _ in range(20):
       n = random.randrange(3, 50+1)
       print >>f, n
+      die = [d / math.sqrt(2) for d in [5, 50, 100, 200, 300, 500]]
+      max_xy = int(random.choice(die))
       ps = set()
       while len(ps) < n:
-        x = random.randrange(-500, 500+1)
-        y = random.randrange(-500, 500+1)
+        x = random.randint(-max_xy, max_xy)
+        y = random.randint(-max_xy, max_xy)
         if (x, y) in ps:
           continue
         print >>f, "%d %d" % (x, y)
