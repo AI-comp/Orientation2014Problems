5d59ee9787ac79ead9d0dd319bbe0490d5c4e450
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2014-05-09 16:18:59 +0900

    issue #13: earthquake in/out

---
+++ earthquake/tests/validator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../lib')
+from checker import Checker
+
+def main():
+  ch = Checker()
+
+  while True:
+    n = int(ch.readLine())
+    if n == -1:
+      break
+    ch.checkRange(n, 3, 50, 'n')
+    ps = []
+    for _ in range(n):
+      x, y = [int(w) for w in ch.readLine().strip().split()]
+      ch.check((x, y) not in ps, "no dup points")
+      ch.checkRange(x, -500, 500, 'x')
+      ch.checkRange(y, -500, 500, 'y')
+      ps.append((x, y))
+
+
+if __name__ == '__main__':
+  main()
