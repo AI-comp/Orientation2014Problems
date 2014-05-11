4af5a0b024c0c0f3c1c29cde36e6a8758fc9cad7
ss0201 <ss0201.777@gmail.com>
2014-05-11 15:06:28 +0900

    Change getio.sh

--- getio.sh
+++ getio.sh
@@ -6,6 +6,8 @@ do
   mkdir "tmp/$dir"
   cp "$dir/rime-out/tests/50-random00.in" "tmp/$dir/in.txt"
   cp "$dir/rime-out/tests/50-random00.diff" "tmp/$dir/out.txt"
+  cp "$dir/rime-out/tests/00-sample1.in" "tmp/$dir/sample-in.txt"
+  cp "$dir/rime-out/tests/00-sample1.diff" "tmp/$dir/sample-out.txt"
   cp "$dir/statement.md" "tmp/$dir/statement.txt"
 done
 
