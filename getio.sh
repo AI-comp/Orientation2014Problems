c430fd9d1cf4c6030c4b61a27020a0a4dbbe2ddf
ss0201 <ss0201.777@gmail.com>
2014-05-14 00:18:00 +0900

    Update getio.sh

--- getio.sh
+++ getio.sh
@@ -4,10 +4,31 @@ mkdir tmp
 for dir in a+b bullet diagonal direction earthquake expression goban hisha lariat masshiro maximum pokemon primes robot social temperature typewriter
 do
   mkdir "tmp/$dir"
-  cp "$dir/rime-out/tests/50-random00.in" "tmp/$dir/in.txt"
-  cp "$dir/rime-out/tests/50-random00.diff" "tmp/$dir/out.txt"
-  cp "$dir/rime-out/tests/00-sample1.in" "tmp/$dir/sample-in.txt"
-  cp "$dir/rime-out/tests/00-sample1.diff" "tmp/$dir/sample-out.txt"
-  cp "$dir/statement.md" "tmp/$dir/statement.txt"
+
+  cp "$dir/rime-out/tests/05-small.in" "tmp/$dir/small-in.txt"
+  cp "$dir/rime-out/tests/05-small.diff" "tmp/$dir/small-out.txt"
+  cp "$dir/rime-out/tests/50-random00.in" "tmp/$dir/large-in.txt"
+  cp "$dir/rime-out/tests/50-random00.diff" "tmp/$dir/large-out.txt"
+
+  statement="tmp/$dir/statement.txt"
+  cp "$dir/statement.md" $statement
+
+  echo "
+Smaple Test Case
+--
+### Input
+\`\`\`" >> $statement
+
+  cat "$dir/rime-out/tests/00-sample1.in" >> $statement
+
+  echo "\`\`\`
+
+### Output
+\`\`\`" >> $statement
+
+  cat "$dir/rime-out/tests/00-sample1.diff" >> $statement
+
+  echo "\`\`\`" >> $statement
+
 done
 
