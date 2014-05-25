7e1f34b43ac6bbb8dcb4f6c66fcfc6d659e918f1
ss0201 <ss0201.777@gmail.com>
2014-05-25 22:53:27 +0900

    Add a sample solution

---
+++ temperature/java-sample/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    while (true) {
+      int N = in.nextInt();
+      if (N == 0) {
+        break;
+      }
+      int[] t = new int[N];
+      for (int i = 0; i < N; i++) {
+        t[i] = in.nextInt();
+      }
+      Arrays.sort(t);
+      int answer = 0;
+      for (int i = 0; i < N - 1; i++) {
+        answer += Math.abs(t[i] - t[i + 1]);
+      }
+      System.out.println(answer);
+    }
+  }
+}
